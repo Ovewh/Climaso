2ae677d2f79cb3ea6a481d005a6414598eebb48c
Ove <ovehaugv@outlook.com>
2022-02-03 09:33:26 +0100

    remove old structure

--- Climaso/workflow/scripts/utils.py
+++
@@ -1,7 +0,0 @@
-import xarray as xr
-
-def load_CMIP_data(path, **dataset_kwargs):
-    if len(path) >1:
-        return xr.open_mfdataset(path, chunks={'time':120}, **dataset_kwargs)
-    else:
-        return xr.open_dataset(path, chunks={'time':120}, **dataset_kwargs)
