3846ce6bf23cee90e9155dff8dc7ca22f5cf12f9
Ove <ovehaugv@outlook.com>
2022-02-10 09:05:38 +0100

    add metadata to the created output

--- workflow/scripts/utils.py
+++ workflow/scripts/utils.py
@@ -1,3 +1,4 @@
+from attr import attr
 import xarray as xr
 
 
@@ -10,3 +11,10 @@ def load_CMIP_data(path, **dataset_kwargs):
         dataset_kwargs.pop('data_vars')
         return xr.open_dataset(path[0], chunks={'time':120}, **dataset_kwargs)
 
+
+def copy_meta_data_CMIP(attrs):
+    remove_keys = ['source','table_info', 'variable_id','tracking_id',
+                    'history','creation_date', 'branch_time_in_child',
+                    'branch_time_in_parent','Conventions']
+    new_attrs = {k: attrs[k] for k in set(list(attrs.keys())) - set(remove_keys)}
+    return new_attrs
