ba7c424fb5dec7ec71244b005adfd3df199d7a1a
Ove <ovehaugv@outlook.com>
2022-02-10 11:36:32 +0100

    remove print

--- workflow/scripts/utils.py
+++ workflow/scripts/utils.py
@@ -5,7 +5,6 @@ import xarray as xr
 def load_CMIP_data(path, **dataset_kwargs):
 
     if len(path) >1:
-        print(path)
         return xr.open_mfdataset(path, chunks={'time':120}, **dataset_kwargs)
     else:
         dataset_kwargs.pop('data_vars')
