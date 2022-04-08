4bab2ee8d7820905b784286b1a50edecd56ffc8e
ovewh <ovehaugv@outlook.com>
2022-04-08 15:22:07 +0200

    add script for calulating climatology

--- workflow/scripts/utils.py
+++ workflow/scripts/utils.py
@@ -7,6 +7,7 @@ def load_CMIP_data(path, **dataset_kwargs):
     if len(path) >1:
         return xr.open_mfdataset(path, chunks={'time':120}, **dataset_kwargs)
     else:
+        print(dataset_kwargs)
         dataset_kwargs.pop('data_vars')
         return xr.open_dataset(path[0], chunks={'time':120}, **dataset_kwargs)
 
@@ -17,3 +18,15 @@ def copy_meta_data_CMIP(attrs):
                     'branch_time_in_parent','Conventions']
     new_attrs = {k: attrs[k] for k in set(list(attrs.keys())) - set(remove_keys)}
     return new_attrs
+
+def transelate_aerocom_helper(wildcards):
+    if wildcards.freq=='9999':
+        freq='clim'
+    elif wildcards.freq=='monthly':
+        freq='Amon'
+    elif wildcards.freq=='yearly':
+        freq='Ayear'
+    else:
+        freq=wildcards.freq
+        
+    return freq
