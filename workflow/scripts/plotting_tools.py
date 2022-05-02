bbc2068bfe07c17e452025a2254370e485487ccb
ovewh <ovehaugv@outlook.com>
2022-05-02 09:54:21 +0200

    reformat black

--- workflow/scripts/plotting_tools.py
+++ workflow/scripts/plotting_tools.py
@@ -1,126 +1,127 @@
 import matplotlib
 from cartopy.mpl.gridliner import LongitudeFormatter, LatitudeFormatter
 import matplotlib.pyplot as plt
-import cartopy.crs as ccrs 
-from matplotlib.ticker import ScalarFormatter,AutoMinorLocator
+import cartopy.crs as ccrs
+from matplotlib.ticker import ScalarFormatter, AutoMinorLocator
+
 
 def global_map(ax):
-    ax.set_extent([-180,180,-90,90], crs=ccrs.PlateCarree())
-    ax.set_xticks([-180,-120,-60,0,60,120,180], crs=ccrs.PlateCarree())
-    ax.set_yticks([-90,-60,-30,0,30,60,90], crs=ccrs.PlateCarree())
+    ax.set_extent([-180, 180, -90, 90], crs=ccrs.PlateCarree())
+    ax.set_xticks([-180, -120, -60, 0, 60, 120, 180], crs=ccrs.PlateCarree())
+    ax.set_yticks([-90, -60, -30, 0, 30, 60, 90], crs=ccrs.PlateCarree())
     lon_formatter = LongitudeFormatter(zero_direction_label=True)
     lat_formatter = LatitudeFormatter()
     ax.xaxis.set_major_formatter(lon_formatter)
     ax.yaxis.set_major_formatter(lat_formatter)
-#     ax.yaxis.set_major_formatter(ScalarFormatter())
-    ax.yaxis.set_minor_locator(  AutoMinorLocator(3))
-    ax.xaxis.set_minor_locator(  AutoMinorLocator(2))
+    #     ax.yaxis.set_major_formatter(ScalarFormatter())
+    ax.yaxis.set_minor_locator(AutoMinorLocator(3))
+    ax.xaxis.set_minor_locator(AutoMinorLocator(2))
     ax.coastlines()
-    ax.set_ylabel(' ')
-    ax.set_xlabel(' ')
+    ax.set_ylabel(" ")
+    ax.set_xlabel(" ")
 
 
-
-
-def create_facet_plot(nplots: int, figsize: tuple=None, 
-                        subplot_kw : dict ={'projection':ccrs.PlateCarree()},
-                        last_axis_plain: bool = False):
+def create_facet_plot(
+    nplots: int,
+    figsize: tuple = None,
+    subplot_kw: dict = {"projection": ccrs.PlateCarree()},
+    last_axis_plain: bool = False,
+):
     layout, default_figsize = _get_layouts(nplots)
     if not figsize:
         figsize = default_figsize
-    fig ,axes = plt.subplot_mosaic(layout,figsize=figsize,subplot_kw=subplot_kw)
+    fig, axes = plt.subplot_mosaic(layout, figsize=figsize, subplot_kw=subplot_kw)
     last = list(axes.keys())[-1]
-    
+
     if last_axis_plain == True:
         pos = axes[last].get_position()
-        axes[last] = fig.add_subplot(axes[last].get_gridspec().nrows, 
-                                        axes[last].get_gridspec().ncols, nplots)
+        axes[last] = fig.add_subplot(
+            axes[last].get_gridspec().nrows, axes[last].get_gridspec().ncols, nplots
+        )
     return fig, axes
-    
-
 
 
 def _get_layouts(nplots: int):
-    figsize=None
-    if nplots==1:
-        layout="""
+    figsize = None
+    if nplots == 1:
+        layout = """
             A
             """
-        figsize=(6,5)
-    elif nplots==2:
-        layout="""
+        figsize = (6, 5)
+    elif nplots == 2:
+        layout = """
             AB
             """
-        figsize=(7,4)    
-    elif nplots==3:
-        layout="""
+        figsize = (7, 4)
+    elif nplots == 3:
+        layout = """
             AABB
             .CC.
         """
-        figsize=(8,6)
-    elif nplots==4:
-        layout="""
+        figsize = (8, 6)
+    elif nplots == 4:
+        layout = """
             AB
             CD
         """
-        figsize=(8,6)
-    elif nplots==5:
-        layout="""
+        figsize = (8, 6)
+    elif nplots == 5:
+        layout = """
             AABB
             CCDD
             .EE.
         """
-        figsize=(14,12)
-    elif nplots==6:
-        layout="""
+        figsize = (14, 12)
+    elif nplots == 6:
+        layout = """
             AB
             CD
             EF
         """
-        figsize=(14,12)
-    elif nplots==7:
-        layout="""
+        figsize = (14, 12)
+    elif nplots == 7:
+        layout = """
             ABC
             DE.
             FG.
         """
-        figsize = (16,10)
-    elif nplots==8:
-        layout="""
+        figsize = (16, 10)
+    elif nplots == 8:
+        layout = """
             ABC
             DEF
             GH.
         """
-        figsize = (16,10)
-    elif nplots==9:
-        layout="""
+        figsize = (16, 10)
+    elif nplots == 9:
+        layout = """
             ABC
             DEF
             GHI
         """
-        figsize = (16,10)
-    elif nplots==10:
-        layout =="""
+        figsize = (16, 10)
+    elif nplots == 10:
+        layout == """
             ABC
             DEF
             GHI
             L
         """
-    elif nplots==11:
-        layout="""
+    elif nplots == 11:
+        layout = """
             ABC
             DEF
             GHI
             LLM
         
         """
-    elif nplots==12:
-        layout="""
+    elif nplots == 12:
+        layout = """
             ABCD
             EFGH
             ILMN
         """
     else:
-        raise(NotImplementedError(f'Layout undefined for nplots = {nplots}'))
+        raise (NotImplementedError(f"Layout undefined for nplots = {nplots}"))
 
     return layout, figsize
--- workflow/scripts/utils.py
+++ workflow/scripts/utils.py
@@ -4,79 +4,92 @@ from pyclim_noresm.aerosol_forcing import merge_exp_ctrl
 from pyclim_noresm.general_util_funcs import global_avg
 import numpy as np
 
+
 def load_CMIP_data(path, **dataset_kwargs):
 
-    if len(path) >1:
-        return xr.open_mfdataset(path, chunks={'time':120}, **dataset_kwargs)
+    if len(path) > 1:
+        return xr.open_mfdataset(path, chunks={"time": 120}, **dataset_kwargs)
     else:
-        dataset_kwargs.pop('data_vars')
-        return xr.open_dataset(path[0], chunks={'time':120}, **dataset_kwargs)
+        dataset_kwargs.pop("data_vars")
+        return xr.open_dataset(path[0], chunks={"time": 120}, **dataset_kwargs)
 
 
 def copy_meta_data_CMIP(attrs):
-    remove_keys = ['source','table_info', 'variable_id','tracking_id',
-                    'history','creation_date', 'branch_time_in_child',
-                    'branch_time_in_parent','Conventions']
+    remove_keys = [
+        "source",
+        "table_info",
+        "variable_id",
+        "tracking_id",
+        "history",
+        "creation_date",
+        "branch_time_in_child",
+        "branch_time_in_parent",
+        "Conventions",
+    ]
     new_attrs = {k: attrs[k] for k in set(list(attrs.keys())) - set(remove_keys)}
     return new_attrs
 
-def regrid_global(ds: xr.DataArray, base_ds: xr.Dataset, lon: int =3, lat: int=2):
+
+def regrid_global(ds: xr.DataArray, base_ds: xr.Dataset, lon: int = 3, lat: int = 2):
     """
-    Return regridded model output to get all the 
+    Return regridded model output to get all the
     models on a common grid.
 
     params
     --------
-        ds: Dataarray containting data field that should be regriddedd 
+        ds: Dataarray containting data field that should be regriddedd
     """
-    ds_out = xe.util.grid_global(lon,lat, cf=True)
-    regridder = xe.Regridder(ds, ds_out, 'conservative')
+    ds_out = xe.util.grid_global(lon, lat, cf=True)
+    regridder = xe.Regridder(ds, ds_out, "conservative")
     ds = regridder(ds, keep_attrs=True)
     return ds
 
 
-def calc_error(da: xr.DataArray, time_dim : str ='year', kind='std'):
+def calc_error(da: xr.DataArray, time_dim: str = "year", kind="std"):
     """
     Calculates the standard error of the mean.
 
     params:
         da: DataArray|Dataset containing timeseries data:
-    
+
     """
-    if isinstance(da,xr.Dataset):
+    if isinstance(da, xr.Dataset):
         da = da[da.variable_id]
     with xr.set_options(keep_attrs=True):
-        if 'lat' in da.dims:
+        if "lat" in da.dims:
             da = global_avg(da)
-        
+
         std = da.std(dim=time_dim)
-        if kind == 'sem' or kind == 'SEM':
-            st_error = std/np.sqrt(len(da[time_dim]))
+        if kind == "sem" or kind == "SEM":
+            st_error = std / np.sqrt(len(da[time_dim]))
         else:
-            st_error=std
-        st_error.attrs['long_name'] = '{} of {}'.format(kind,st_error.attrs['long_name'])
-       
+            st_error = std
+        st_error.attrs["long_name"] = "{} of {}".format(
+            kind, st_error.attrs["long_name"]
+        )
 
         return st_error
+
 
 def calc_relative_change(ds_ctrl: xr.Dataset, ds_exp: xr.Dataset):
     """
     Calculate the relative change of a diagnostic between
-    the control and experiment. The data is on model level 
+    the control and experiment. The data is on model level
     it is integrated vertically.
 
     params
     ------
         ds_ctrl: dataset containing the control experiment
         ds_exp: dataset containing the perturbed experiment
-    
+
     returns
     --------
         rel_diff: dataset containing the relative difference
     """
-    return _calc_change(ds_ctrl, ds_exp,rel_change=True) 
+    return _calc_change(ds_ctrl, ds_exp, rel_change=True)
 
-def calc_abs_change(ds_ctrl:xr.Dataset, ds_exp: xr.Dataset):
+
+def calc_abs_change(ds_ctrl: xr.Dataset, ds_exp: xr.Dataset):
     """
     Calculated absolute change of diagnostic between
     the control and experiment. Data that is on model
@@ -86,52 +99,60 @@ def calc_abs_change(ds_ctrl:xr.Dataset, ds_exp: xr.Dataset):
     -------
         ds_ctrl: dataset containing the control experiment
         ds_exp: dataset containing the perturbed experiment
-    
+
     """
     return _calc_change(ds_ctrl, ds_exp, rel_change=False)
 
-def _calc_change(ds_ctrl:xr.Dataset, ds_exp:xr.Dataset, rel_change: bool =False):
+
+def _calc_change(ds_ctrl: xr.Dataset, ds_exp: xr.Dataset, rel_change: bool = False):
 
     vName = ds_ctrl.variable_id
     dvars = set(list(ds_ctrl.data_vars))
-    keep_vars = set(['lev_bounds','time_bounds', 'lon_bnds','lat_bnds',vName])
-    
+    keep_vars = set(["lev_bounds", "time_bounds", "lon_bnds", "lat_bnds", vName])
+
     with xr.set_options(keep_attrs=True):
-        ds_ctrl = ds_ctrl.drop_vars(dvars-keep_vars).squeeze()
-        ds_exp = ds_exp.drop_vars(dvars-keep_vars).squeeze()
+        ds_ctrl = ds_ctrl.drop_vars(dvars - keep_vars).squeeze()
+        ds_exp = ds_exp.drop_vars(dvars - keep_vars).squeeze()
         ds = merge_exp_ctrl(ds_ctrl, ds_exp)
 
-        if 'lev' in ds_ctrl.dims:
-            da_ctrl = ds[f'control_{vName}'].sum(dim='lev')
-            da_exp = ds[vName].sum(dim='lev')
+        if "lev" in ds_ctrl.dims:
+            da_ctrl = ds[f"control_{vName}"].sum(dim="lev")
+            da_exp = ds[vName].sum(dim="lev")
         else:
-            da_ctrl = ds[f'control_{vName}']
+            da_ctrl = ds[f"control_{vName}"]
             da_exp = ds[vName]
-        if 'year' in da_ctrl.dims:
-            da_ctrl = da_ctrl.mean(dim='year')
-            da_exp = da_exp.mean(dim='year')
+        if "year" in da_ctrl.dims:
+            da_ctrl = da_ctrl.mean(dim="year")
+            da_exp = da_exp.mean(dim="year")
         if rel_change:
-            diff = ((da_exp-da_ctrl)/da_ctrl)*100
-            diff.attrs['units'] = '%'
-            diff.attrs['long_name'] = 'Relative change of {}'.format(da_exp.attrs['long_name']) 
+            diff = ((da_exp - da_ctrl) / da_ctrl) * 100
+            diff.attrs["units"] = "%"
+            diff.attrs["long_name"] = "Relative change of {}".format(
+                da_exp.attrs["long_name"]
+            )
             diff = diff.to_dataset(name=vName)
             diff.attrs = {**diff.attrs, **ds_exp.attrs}
         else:
-            diff = da_exp-da_ctrl
-            diff.attrs['long_name'] = 'Difference between experiment and control of {}'.format(da_exp.attrs['long_name'])
+            diff = da_exp - da_ctrl
+            diff.attrs[
+                "long_name"
+            ] = "Difference between experiment and control of {}".format(
+                da_exp.attrs["long_name"]
+            )
             diff = diff.to_dataset(name=vName)
             diff.attrs = {**diff.attrs, **ds_exp.attrs}
     return diff
 
+
 def transelate_aerocom_helper(wildcards):
     print(wildcards)
-    if wildcards.freq=='2010':
-        freq='clim'
-    elif wildcards.freq=='monthly':
-        freq='Amon'
-    elif wildcards.freq=='yearly':
-        freq='Ayear'
+    if wildcards.freq == "2010":
+        freq = "clim"
+    elif wildcards.freq == "monthly":
+        freq = "Amon"
+    elif wildcards.freq == "yearly":
+        freq = "Ayear"
     else:
-        freq=wildcards.freq
-        
+        freq = wildcards.freq
+
     return freq
